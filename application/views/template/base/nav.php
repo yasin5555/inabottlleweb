9db06ec53ed02620f3e6d34d719f655e3df9aac0
jeremy <jerem.negaret@gmail.com>
2017-04-23 09:57:59 +0200

    corrective pre push yassine

--- application/views/template/base/nav.php
+++ application/views/template/base/nav.php
@@ -1,15 +1,10 @@
-			
-            
-            
-       
-
 
             <div>
 				<img src="http://img4.hostingpics.net/pics/249732inabotlltlemin.png">
 			</div>
 			<ul>
 			  <li><a href="<?= base_url("login/index")?>">&#35;In-a-bottle</a></li>
-			  <li><a href="<?= base_url("/inabottle>/index")?>">Default view</a></li>
+			  <li><a href="<?= base_url("inabottle>/index")?>">Default view</a></li>
 			  <li class="dropdown">
 			    <a href="javascript:void(0)" class="dropbtn">Filtres &#35;</a>
 			    <div class="dropdown-content">
